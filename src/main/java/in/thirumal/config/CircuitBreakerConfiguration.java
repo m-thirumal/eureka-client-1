b6126179341b56d107f324d4535b4ba82359dcba
Thirumal <m.thirumal@hotmail.com>
2020-03-05 19:29:31 +0530

    Added Resilience

---
+++ src/main/java/in/thirumal/config/CircuitBreakerConfiguration.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package in.thirumal.config;
+
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
+/**
+ * @author thirumal
+ *
+ */
+@Configuration
+public class CircuitBreakerConfiguration {
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+	    return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+	        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+	        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(1)).build())
+	        .build());
+	}
+	
+}
---
+++ src/test/java/in/thirumal/resilience/GatewayCircuitBreakerTest.java
@@ -0,0 +1,5 @@
+package in.thirumal.resilience;
+
+public class GatewayCircuitBreakerTest {
+
+}
