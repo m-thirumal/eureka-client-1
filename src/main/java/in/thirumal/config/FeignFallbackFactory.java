244341b37810582a6bbc6e3696d823f58a667d2d
Thirumal <m.thirumal@hotmail.com>
2021-03-09 17:53:56 +0530

    upgraded

--- src/main/java/in/thirumal/config/FeignFallbackFactory.java
+++ src/main/java/in/thirumal/config/FeignFallbackFactory.java
@@ -4,36 +4,38 @@
 package in.thirumal.config;
 
 import java.util.IntSummaryStatistics;
-import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Component;
-
-import feign.FeignException;
-import feign.hystrix.FallbackFactory;
 
 /**
  * @author thirumal
  *
  */
 @Component
-public class FeignFallbackFactory implements FallbackFactory<ClientFeign> {
+public class FeignFallbackFactory implements ClientFeign {
+
+//	@Override
+//	public ClientFeign create(Throwable cause) {
+//		String httpStatus = cause instanceof FeignException ? Integer.toString(((FeignException) cause).status()) : "";
+//		return new ClientFeign() {
+//			@Override
+//			public IntSummaryStatistics getSummaryStatistics() {
+//				System.out.println(httpStatus);
+//				if (cause instanceof FeignException) {
+//					System.out.println("Client is not available or something");
+//					return IntStream.range(0, 0).summaryStatistics();
+//				} else {
+//					System.out.println("Other exception");
+//					return IntStream.range(0, 0).summaryStatistics();
+//				}
+//			}
+//		};
+//	}
 
 	@Override
-	public ClientFeign create(Throwable cause) {
-		String httpStatus = cause instanceof FeignException ? Integer.toString(((FeignException) cause).status()) : "";
-		return new ClientFeign() {
-			@Override
-			public IntSummaryStatistics getSummaryStatistics() {
-				System.out.println(httpStatus);
-				if (cause instanceof FeignException) {
-					System.out.println("Client is not available or something");
-					return IntStream.range(0, 0).summaryStatistics();
-				} else {
-					System.out.println("Other exception");
-					return IntStream.range(0, 0).summaryStatistics();
-				}
-			}
-		};
+	public IntSummaryStatistics getSummaryStatistics() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 //
